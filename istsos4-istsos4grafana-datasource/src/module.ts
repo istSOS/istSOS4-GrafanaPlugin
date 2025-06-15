1e8999969f6db1745c1974e9d4048de690419dec
MostafaMagdyy <mustafa.rahman02@eng-st.cu.edu.eg>
2025-06-15 21:43:50 +0300

    Adding Query UI and Query Preview

--- istsos4-istsos4grafana-datasource/src/module.ts
+++ istsos4-istsos4grafana-datasource/src/module.ts
@@ -2,8 +2,8 @@ import { DataSourcePlugin } from '@grafana/data';
 import { DataSource } from './datasource';
 import { ConfigEditor } from './components/ConfigEditor';
 import { QueryEditor } from './components/QueryEditor';
-import { MyQuery, MyDataSourceOptions } from './types';
+import { IstSOS4Query, MyDataSourceOptions } from './types';
 
-export const plugin = new DataSourcePlugin<DataSource, MyQuery, MyDataSourceOptions>(DataSource)
+export const plugin = new DataSourcePlugin<DataSource, IstSOS4Query, MyDataSourceOptions>(DataSource)
   .setConfigEditor(ConfigEditor)
   .setQueryEditor(QueryEditor);
